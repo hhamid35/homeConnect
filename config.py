b89c7e8b9297981d6613beed493c8ac8eeb282ea
hhamid35 <32471959+hhamid35@users.noreply.github.com>
2020-01-02 17:05:15 -0500

    change to config to make server visible to other devices on the network

--- config.py
+++ config.py
@@ -4,5 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG = True
+    HOST = '0.0.0.0'
+    PORT = '80'
     SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(basedir, 'app.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
