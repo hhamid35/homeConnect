d9e967b3f4d3a32a26a38091d39e6ed20cfa0306
hhamid35 <32471959+hhamid35@users.noreply.github.com>
2020-01-02 17:19:46 -0500

    host address with port specified in run.py instead of config.py

--- config.py
+++ config.py
@@ -4,7 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG = True
-    HOST = '0.0.0.0'
-    PORT = '80'
     SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(basedir, 'app.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from web_portal import app
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port='80')
 
 
     # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
