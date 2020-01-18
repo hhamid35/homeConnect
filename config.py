6f3a1d858a72c1da3f6e49a612a4483da7fb63aa
hhamid35 <32471959+hhamid35@users.noreply.github.com>
2020-01-18 15:14:25 -0500

    correction to config file

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ project_dir = os.path.dirname(os.path.abspath(__file__))
 
 class Config(object):
     DEBUG = True
-    SERVER_NAME = 'localhost:5000'
+    SERVER_NAME = 'localhost:80'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(project_dir, 'homeConnect.db'))
