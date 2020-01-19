eba9e7b0a011460a269327045cf67ecbcb56424c
hhamid35 <32471959+hhamid35@users.noreply.github.com>
2020-01-19 15:34:06 -0500

    /deviceAction

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import app, socketio
 
 if __name__ == '__main__':
-    # socketio.run(app, host='0.0.0.0', port='80', use_reloader=False)
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port='80', use_reloader=False)
+    # socketio.run(app)
